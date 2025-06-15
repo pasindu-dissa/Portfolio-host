8b2d0a939a3edc153c11aa3c6469be7f8d9d5e3b
Pasindu Dissanayake <pasindudissanayake035@gmail.com>
2025-06-15 15:19:21 +0530

    Update navbar.tsx - connect email

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -56,7 +56,7 @@ const Navbar: React.FC = () => {
         <div className="hidden min-[701px]:flex items-center gap-6">
           <MdLightMode className="cursor-pointer text-3xl" />
           <Link
-            href="#connect"
+            href="mailto:pasindudissanayake035@gmail.com?subject=Let's%20Connect&body=Hi,%20I'd%20like%20to%20get%20in%20touch!"
             className="border-2 p-1 cursor-pointer transition-all duration-200 ease-in-out hover:bg-white hover:border-white hover:text-black"
           >
             let&apos;s connect
@@ -126,7 +126,7 @@ const Navbar: React.FC = () => {
         {/* Footer Link */}
         <div className="mt-auto">
           <Link
-            href="#connect"
+            href="mailto:pasindudissanayake035@gmail.com?subject=Let's%20Connect&body=Hi,%20I'd%20like%20to%20get%20in%20touch!"
             onClick={closeMenu}
             className="flex items-center gap-4 p-3 rounded-lg text-white/80 hover:bg-white/10 hover:text-white transition-all"
           >
